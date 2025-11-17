c92bf97625a16bcd37d4189e4a1f42d72a7e392e
khushi <khushi21091509@gmail.com>
2025-11-17 13:27:16 +0530

    changes in server.js to correctly serve the React build files.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const path = require("path");
-
+// Serve React build
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 // Middlewares (always first)
 app.use(cors());
 app.use(express.json());
@@ -31,14 +32,14 @@ app.post("/api/recommend", async (req, res) => {
   }
 });
 
-// Serve React build
-app.use(express.static(path.join(__dirname, "../client/build")));
 
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+app.use((req, res, next) => {
+  if (
+    req.path.startsWith('/api') ||
+    req.path.match(/\.[^\/]+$/)
+  ) return next();
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
-
-
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
